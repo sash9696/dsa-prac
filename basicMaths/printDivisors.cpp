8b35f24f83462dce74f93bd64c139902d5b75b58
sash9696 <sahilchopra838@gmail.com>
2024-05-29 08:47:11 +0530

    printDivisors optimized approach

--- basicMaths/printDivisors.cpp
+++ basicMaths/printDivisors.cpp
@@ -15,6 +15,28 @@ using namespace std;
 // //naive approach
 // //TC => O(N)
 
+void printDivisors(int x){
+
+    vector<int> ls;
+
+    // for(int i = 1; i <= sqrt(x); i++){
+    for(int i = 1; i*i <= x; i++){
+        if(x % i == 0){
+            ls.push_back(i);
+
+            if(x / i != i){
+                ls.push_back(x/i);
+            }
+        }
+    }
+    //optimized approach
+//TC => O(sqrt(N))
+
+    sort(ls.begin(), ls.end());
+//TC => O(no of factors * log(no of factors))
+    for(auto it: ls) cout << it << " ";
+    //TC => O(no of factors)
+}
 
 
 int main(){
