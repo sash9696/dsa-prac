fe3728b5f8cc9cc8fddde3972e195408b85516c3
sash9696 <sahilchopra838@gmail.com>
2024-05-30 12:46:05 +0530

    basic all char hashing

--- hashing/charArrayHashing.cpp
+++ hashing/charArrayHashing.cpp
@@ -3,6 +3,44 @@ using namespace std;
 
 //lowercase letters
 
+// int main(){
+
+//     string s;
+
+//     cin >> s;
+
+//     cout<<s<<endl;
+
+//     //precompute
+//     int hash[26] = {0};
+
+
+//     for (int i = 0; i < s.size(); i++){
+//             hash[s[i] - 'a'] ++;
+
+//     }
+
+//     int q;
+//     cin >> q;
+
+//     while(q--){
+//         char ch;
+//         cin >> ch;
+
+//         //fetch
+
+//         cout << hash[ch - 'a'] << endl;
+
+//     }
+
+    
+
+//     return 0;
+// }
+
+
+//for all chars if nothing is mentione din the question
+//total => 256
 int main(){
 
     string s;
@@ -12,11 +50,11 @@ int main(){
     cout<<s<<endl;
 
     //precompute
-    int hash[26] = {0};
+    int hash[256] = {0};
 
 
     for (int i = 0; i < s.size(); i++){
-            hash[s[i] - 'a'] ++;
+            hash[s[i]] ++;
 
     }
 
@@ -29,7 +67,7 @@ int main(){
 
         //fetch
 
-        cout << hash[ch - 'a'] << endl;
+        cout << hash[ch] << endl;
 
     }
 
@@ -37,3 +75,4 @@ int main(){
 
     return 0;
 }
+
