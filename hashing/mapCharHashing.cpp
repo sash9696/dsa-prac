6326763d0f8ab6365ff1d07b2f046b36ca3dd575
sash9696 <sahilchopra838@gmail.com>
2024-05-31 23:33:23 +0530

    map char hashing

---
+++ hashing/mapCharHashing.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <map>
+using namespace std;
+
+int main()
+{
+
+    int n;
+    cin >> n;
+    int arr[n];
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    // precompute
+    map<int, int> mpp;
+
+    for (int i = 0; i < n; i++)
+    {
+        mpp[arr[i]]++;
+    }
+
+    for (auto it : mpp)
+    {
+        cout << it.first << "-->" << it.second << endl;
+    }
+
+    int q;
+    cin >> q;
+
+    while (q--)
+    {
+        int number;
+        cin >> number;
+
+        // fetch
+
+        cout << mpp[number] << endl;
+    }
+
+    return 0;
+}
